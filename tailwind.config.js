bc8fd3a1621ae366ce740e836bcdbb6a175cd789
anupkumarreddy <kumarreddy002@gmail.com>
2026-04-29 21:23:27 +0530

    Add the new styling to the base template

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     './**/templates/**/*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Google Sans"', 'Roboto', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        ai: {
+          bg: '#131314',
+          surface: '#1e1f20',
+          surface2: '#282a2c',
+          border: '#3c4043',
+          text: '#e8eaed',
+          muted: '#9aa0a6',
+          blue: '#8ab4f8',
+          primary: '#a8c7fa',
+        },
+      },
+    },
   },
   plugins: [],
 }

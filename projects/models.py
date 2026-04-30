623e2bc780dabb0aca2fe4d45aac09f1c647b59c
anupkumarreddy <kumarreddy002@gmail.com>
2026-04-30 16:56:43 +0530

    Add project category.

--- projects/models.py
+++ projects/models.py
@@ -4,10 +4,31 @@ from django.utils.text import slugify
 from common.models import TimeStampedModel
 from common.choices import ProjectStatus
 
+
+class ProjectCategory(TimeStampedModel):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'project categories'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
 class Project(TimeStampedModel):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, blank=True)
     description = models.TextField(blank=True)
+    category = models.ForeignKey(ProjectCategory, on_delete=models.SET_NULL, null=True, blank=True, related_name='projects')
     owner = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, null=True, blank=True, related_name='owned_projects')
     status = models.CharField(max_length=20, choices=ProjectStatus.choices, default=ProjectStatus.ACTIVE)
     repository_url = models.URLField(blank=True)
